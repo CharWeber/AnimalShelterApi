641d1d24de931e171d88945b1e6995a58fab7ca5
Charlie Weber <charlestweber@gmail.com>
2021-10-29 11:33:50 -0700

    update seeded data

--- Shelter/Models/ShelterContext.cs
+++ Shelter/Models/ShelterContext.cs
@@ -15,7 +15,9 @@ namespace Shelter.Models
         builder.Entity<Animal>()
             .HasData(
                 new Animal { AnimalId = 1, Name = "Mr. Meowmers", Species = "cat", Age = 2, Gender = "Male" },
-                new Animal { AnimalId = 2, Name = "Friendo", Species = "dog", Age = 1, Gender = "Female" }
+                new Animal { AnimalId = 2, Name = "Friendo", Species = "dog", Age = 1, Gender = "Female" },
+                new Animal { AnimalId = 2, Name = "Buster", Species = "dog", Age = 2, Gender = "Male" },
+                new Animal { AnimalId = 2, Name = "ShelterCat.exe", Species = "cat", Age = 3, Gender = "Female" },
             );
         }
 
